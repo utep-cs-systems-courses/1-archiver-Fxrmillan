e8137100a78667698bdfa3bafa9740ada19dfae0
felix <frmillan13@outlook.com>
2023-02-08 16:51:53 -0600

    added some more test code program now finds null

--- src/MyTar.py
+++ src/MyTar.py
@@ -5,14 +5,19 @@ import sys
 from os import read, write
 
 filesToPrint = sys.argv[1:]
+print(len(filesToPrint))
 ifd = filesToPrint[0]
 
 temp = os.open(ifd, os.O_RDWR)
 ibuf = read(temp, 100)
 while len(ibuf):
     sbuf = ibuf.decode()
+    if sbuf.find("/x") != -1:
+        print(sbuf)
+        print("found /x")
+    print(sbuf)
     ibuf = read(temp, 100)
-os.write(temp, "AWOOGA".encode())
+os.write(temp, "/x".encode())
 # summary to stderr
 
 os.close(temp)
